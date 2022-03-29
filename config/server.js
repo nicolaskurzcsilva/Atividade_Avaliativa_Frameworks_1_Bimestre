e997853f6f382e065e4cc6fe3f57514915eaefa4
Nícolas Kurz <99290276+NicolasKurz67@users.noreply.github.com>
2022-03-29 15:52:32 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 const express = require("express");//importação do motor de telas "ejs"
-const app = express();
-app.set("view-engine", "ejs");
-app.set("views", "./app/views");
+const app = express();//execução do "express"
+app.set("view-engine", "ejs");//configuração do ejs como motor de telas
+app.set("views", "./app/views");//alteração do diretório da pasta views
 
 module.exports = app;//exportando o "ejs" para a const app
