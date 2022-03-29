bdf808e3e8a13f7394c8084074f111f8f8b3dc24
Nícolas Kurz <99290276+NicolasKurz67@users.noreply.github.com>
2022-03-29 17:11:07 -0300

    Update noticias.js

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,12 +1,11 @@
-module.exports=(app)=>{//modularização da rota "/noticias"
+module.exports=(app)=>{//exportação da rota "/noticias"
     app.get("/noticias", (req, res)=>{
         const dbConnection = require("../../config/dBConnection");//importação da conexão com o banco de dados que está modularizada em outro diretório
         const connection = dbConnection();//criação de uma variável dbConnection e executando ela
-            connection.query("select * from noticias", (error, result)=>{//verificação da requisição para caso haja algum erro
-                if(error){
+            connection.query("select * from noticias", (error, result)=>{
+                if(error){//verificação da requisição para caso haja algum erro
                     console.log(error);
                 }
-                res.render("noticias/noticias.ejs", {noticias:result});//renderização da rota "/noticias"
-            });
-    });
+                res.render("noticias/noticias.ejs", {noticias:result});//responder à requisição "/noticias" com a renderização da tela noticias/noticias.ejs
+    });    
 }
