84ea15602f9bf3fdd61e39ca42992e067a22b7df
Nícolas Kurz <99290276+NicolasKurz67@users.noreply.github.com>
2022-03-29 16:07:29 -0300

    Update noticias.js

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,7 +1,7 @@
 module.exports=(app)=>{//modularização da rota "/noticias"
     app.get("/noticias", (req, res)=>{
         const dbConnection = require("../../config/dBConnection");//importação da conexão com o banco de dados que está modularizada
-        const connection = dbConnection();
+        const connection = dbConnection();//criação de uma conexão com ol portal_noticias
             connection.query("select * from noticias", (error, result)=>{//verificação da requisição para caso haja algum erro
                 if(error){
                     console.log(error);
