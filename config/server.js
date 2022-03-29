f9043190abdb71869e84d43ed5b73df6f7594d31
Nícolas Kurz <99290276+NicolasKurz67@users.noreply.github.com>
2022-03-29 16:12:02 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -3,4 +3,4 @@ const app = express();//execução do "express"
 app.set("view-engine", "ejs");//configuração do ejs como motor de telas
 app.set("views", "./app/views");//alteração do diretório da pasta views
 
-module.exports = app;//exportando o "ejs" para a const app
+module.exports = app;//exportação do "ejs" para a const app
