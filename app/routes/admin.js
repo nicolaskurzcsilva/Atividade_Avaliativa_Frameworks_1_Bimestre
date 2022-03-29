1f143b251e39e0e05f3d301dc0d99a19b342c7eb
Nícolas Kurz <99290276+NicolasKurz67@users.noreply.github.com>
2022-03-29 15:54:31 -0300

    Update admin.js

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,5 +1,5 @@
 module.exports=(app)=>{//modularização da rota "/formulario_inclusao_noticia"
     app.get("/formulario_inclusao_noticia", (req,res)=>{
-        res.render("admin/form_add_noticia.ejs");
+        res.render("admin/form_add_noticia.ejs");//responder à requisição "/formulario_inclusao_noticia" com a tela admin/form_add_noticia
     });    
 }
