cd29773530ed0804fa98a299741c7cbb90c91b0c
Nícolas Kurz <99290276+NicolasKurz67@users.noreply.github.com>
2022-03-29 16:15:57 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const app = require("./config/server.js");//importação do módulo "ejs"
+const app = require("./config/server.js");//importação do módulo express do arquivo server.js
 const rotaAdmin = require("./app/routes/admin.js")(app);//importação da rota "/formulario_inclusao_noticia"
 const rotaHome = require("./app/routes/home.js")(app);//importação da rota principal do projeto "localhost:3000/"
 const rotaNoticias = require("./app/routes/noticias.js")(app);//importação da rota "/noticias"
