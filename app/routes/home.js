d90ade4cff95387689bd06afce4c59b4afdbd1e2
Nícolas Kurz <99290276+NicolasKurz67@users.noreply.github.com>
2022-03-29 17:06:13 -0300

    Update home.js

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,4 @@
-module.exports=(app)=>{//modularização da rota principal do projeto "localhost:3000/"
+module.exports=(app)=>{//exportação da rota principal do projeto "localhost:3000/"
     app.get("/", (req,res)=>{
         res.render("home/index.ejs");
     });
