b37e20c7ac6f1b643ff26c53d755b2a581995e26
Nícolas Kurz <99290276+NicolasKurz67@users.noreply.github.com>
2022-03-29 15:53:24 -0300

    Update dBConnection.js

--- config/dBConnection.js
+++ config/dBConnection.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("mysql");//importação do módulo mysql
 
 module.exports = ()=>{//modularização da conexão com o banco de dados
     return mysql.createConnection({
@@ -6,5 +6,5 @@ module.exports = ()=>{//modularização da conexão com o banco de dados
         user:"root",
         password:"root",
         database:"portal_noticias"
-    });
+    });//criação da conexão com o banco de dados "portal_noticias"
 }
